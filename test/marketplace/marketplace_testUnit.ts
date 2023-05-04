73f441e5c1bbe058330f7b846916e0c266f8a191
Pedro Machado Leiva <pyuris22@gmail.com>
2023-05-04 02:33:16 -0400

    test(list): It is necessary to create a Wizard ERC1155 collection for testing list and liquidity function.

--- test/marketplace/marketplace_testUnit.ts
+++ test/marketplace/marketplace_testUnit.ts
@@ -1,12 +1,20 @@
 import {deployments, ethers} from "hardhat";
 import {assert, expect} from "chai";
 import {Marketplace} from "../../scripts/marketplace";
+import { Signer } from "ethers";
 
 describe("Testing Marketplace Smart Contract", () => {
+    let signer:Signer;
     
     beforeEach(async () => {
         await deployments.fixture("Marketplace");
+        await defaultSigner();
     });
+
+    async function defaultSigner() {
+        const signers = await ethers.getSigners();
+        signer = signers[0];
+    }
 
     it("Marketplace should be deployed, therefore, it has linked an address", async () =>  {
         const zeroAddress = ethers.constants.AddressZero;
@@ -17,7 +25,7 @@ describe("Testing Marketplace Smart Contract", () => {
 
     it("A NFT should be listed using list function.", async () => {
         const marketplaceDeployment = await deployments.get("Marketplace");
-        const marketplace = new Marketplace(marketplaceDeployment.address);
+        const marketplace = new Marketplace(marketplaceDeployment.address, signer);
         const currentLiquidity = await marketplace.liquidity();
         const collectionAddress = "";
         const nftId = "";
