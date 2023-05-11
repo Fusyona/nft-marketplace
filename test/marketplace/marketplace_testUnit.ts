889090dc5a57435c7211143aaa349f1b08f25e69
Pedro Machado Leiva <pyuris22@gmail.com>
2023-05-11 12:48:45 -0400

    test: It was added other tests related to list function, in order to check the list requirements.

--- test/marketplace/marketplace_testUnit.ts
+++ test/marketplace/marketplace_testUnit.ts
@@ -54,5 +54,59 @@ describe("Testing Marketplace Smart Contract", () => {
         assert.equal(_tvlAfterList, _tvlBeforeList + 1, "_tvlAfterList should be increased plus one");
     });
 
+    it("If there are more than two NFT in the marketplace the function totalOfNFTListed should returns 2", async () => {
+        
+        const marketplace = new Marketplace(marketplaceDeployment.address, signer);
+        
+        const tvlBeforeList = await marketplace.totalOfNFTListed();
+        const _tvlBeforeList = BN.from(tvlBeforeList).toNumber();
+
+        const collectionAddress = mockERC1155CollectionDeployment.address;
+        const nftId1 = "1";
+        const nftId2 = "2";
+        const price = ethers.utils.parseEther("1");
+
+        const mockCollection = await ethers.getContractAt("ERC1155", collectionAddress);
+        
+        await mockCollection.setApprovalForAll(marketplace.contractAddress, true);
+        
+        await marketplace.list(collectionAddress, nftId1, price.toString());  
+        await marketplace.list(collectionAddress, nftId2, price.toString());  
+    
+
+        const tvlAfterList = await marketplace.totalOfNFTListed();
+        const _tvlAfterList = BN.from(tvlAfterList).toNumber();
+       
+        assert.equal(_tvlAfterList, _tvlBeforeList + 2, "_tvlAfterList should be increased plus one");
+    });
+
+    it("An exception should revert if an user try to list twice the same NFT.", async () => {
+        const marketplace = new Marketplace(marketplaceDeployment.address, signer);
+        const collectionAddress = mockERC1155CollectionDeployment.address;
+        const nftId1 = "1";
+        const price = ethers.utils.parseEther("1");
+        
+        const mockCollection = await ethers.getContractAt("ERC1155", collectionAddress);
+        
+        await mockCollection.setApprovalForAll(marketplace.contractAddress, true);
+
+        await marketplace.list(collectionAddress, nftId1, price.toString());  
+        await expect (marketplace.list(collectionAddress, nftId1, price.toString())).to.be.revertedWith(
+            'Marketplace: Error when listed'
+        );  
+    
+    });
+
+    it("If an user owner of a NFT try to list that nft before it grants to Marketplace rigths over its token, then an exeception should be throwed.", async () => {
+        const marketplace = new Marketplace(marketplaceDeployment.address, signer);
+        const collectionAddress = mockERC1155CollectionDeployment.address;
+        const nftId1 = "1";
+        const price = ethers.utils.parseEther("1");
+        
+        await expect(marketplace.list(collectionAddress, nftId1, price.toString())).to.be.revertedWith(
+            "ERC1155: caller is not token owner or approved"
+            );  
+    })
+
 
 });
