90f686c68fd582fc105ae953376027f85436ef50
Pedro Machado Leiva <pyuris22@gmail.com>
2023-05-17 10:36:32 -0400

    refactor: Some variable names in the MathFees library were changed to improve its readability.

--- contracts/MathFees.sol
+++ contracts/MathFees.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 library MathFees {
 
-function _verifyFeeRatioBounds(int128 value) internal pure returns (bool) {
-    require(value > 0 && value <= _getMaxValueStoredInUint128(), "Math: Value of feeRatio is out of bound.");
-    require(_getRemainderOfUint128(value) == 0, "Math: You need to encode the argument as: percentage*100*2^64.");
+function _verifyFeeRatioBounds(int128 _percentageMultipliedBy2Up64AndTwoDecimals) internal pure returns (bool) {
+    require(_percentageMultipliedBy2Up64AndTwoDecimals > 0 && _percentageMultipliedBy2Up64AndTwoDecimals <= _getMaxValueStoredInUint128(), "Math: Value of feeRatio is out of bound.");
+    require(_getRemainderOfUint128(_percentageMultipliedBy2Up64AndTwoDecimals) == 0, "Math: You need to encode the argument as: percentage*100*2^64.");
     return true;
     }
 
@@ -15,21 +15,21 @@ function _getMaxValueStoredInUint128() internal pure returns(int128) {
     return int128(0x7fffffffffffffffffffffffffffffff);
     }
 
-function _getRemainderOfUint128(int128 value) internal pure returns(int128) {
-    require(value > 0, "Math: Value should be greater than 0.");
+function _getRemainderOfUint128(int128 _percentageMultipliedBy2Up64AndTwoDecimals) internal pure returns(int128) {
+    require(_percentageMultipliedBy2Up64AndTwoDecimals > 0, "Math: Value should be greater than 0.");
     int128 denominator = 2**64;
-    return value % denominator;
+    return _percentageMultipliedBy2Up64AndTwoDecimals % denominator;
     }
 
-function _computeFeeRatio(int128 value) internal pure returns (int128) {
-    return ((value / _twoDecimalsForPercentageInput()) /int128(100));
+function _computeFeeRatio(int128 _percentageMultipliedBy2Up64AndTwoDecimals) internal pure returns (int128) {
+    return ((_percentageMultipliedBy2Up64AndTwoDecimals / _twoDecimalsPerPercentageInput()) /int128(100));
     }
 
 function _2percent() internal pure returns(int128) {
     return (int128(2) * int128(2**64))/ int128(100);
     }
 
-function _twoDecimalsForPercentageInput() private pure returns (int128) {
+function _twoDecimalsPerPercentageInput() private pure returns (int128) {
     return int128(100);
     }
 
