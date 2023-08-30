d3069cbd987af97891712f0c28395f103dc7d143
MakeMake23 <andy.ledesma.garcia@gmail.com>
2023-08-30 16:04:27 -0400

    test: royalty support in Erc20PaymentMarketplace
    Issues: #55 #57

--- test/royalty-testUnits.ts
+++ test/royalty-testUnits.ts
@@ -3,8 +3,9 @@ import { deployments, ethers } from "hardhat";
 import {
     FusyERC721CollectionWithRoyaltySupport,
     Marketplace,
-    IERC721,
     MsgValuePaymentMarketplace,
+    Erc20PaymentMarketplace,
+    IERC20,
 } from "../typechain-types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber, ContractTransaction } from "ethers";
@@ -16,9 +17,10 @@ describe("Royalty support tests", () => {
     let seller: SignerWithAddress;
     let buyer: SignerWithAddress;
 
-    let marketplace: MsgValuePaymentMarketplace;
-    let collectionContractWithoutERC2981: IERC721;
+    let msgValueMarketplace: MsgValuePaymentMarketplace;
+    let erc20Marketplace: Erc20PaymentMarketplace;
     let collectionContractWithERC2981: FusyERC721CollectionWithRoyaltySupport;
+    let payToken: IERC20;
 
     let nftId = 1;
     const nftPrice = ethers.utils.parseEther("10");
@@ -35,179 +37,344 @@ describe("Royalty support tests", () => {
             "MockERC721Collection",
             "FusyERC721CollectionWithRoyaltySupport",
             "MsgValuePaymentMarketplace",
+            "MockERC20",
+            "Erc20PaymentMarketplace",
         ]);
 
-        collectionContractWithoutERC2981 = await ethers.getContract(
-            "MockERC721Collection"
-        );
         collectionContractWithERC2981 = await ethers.getContract(
             "FusyERC721CollectionWithRoyaltySupport"
         );
-        marketplace = await ethers.getContract("MsgValuePaymentMarketplace");
+        msgValueMarketplace = await ethers.getContract(
+            "MsgValuePaymentMarketplace"
+        );
+        payToken = await ethers.getContract("MockERC20");
+        erc20Marketplace = await ethers.getContract("Erc20PaymentMarketplace");
     });
 
-    async function expectToChangeEtherBalance(
-        trxPromise: (marketplace: Marketplace) => Promise<ContractTransaction>,
-        target: Address,
-        balanceChange: number | BigNumber,
-        errorMargin: number | BigNumber
-    ) {
-        const balanceBefore = await ethers.provider.getBalance(target);
-        await trxPromise(marketplace);
-        const balanceAfter = await ethers.provider.getBalance(target);
+    describe("MsgValuePaymentMarketplace", () => {
+        beforeEach(async () => {
+            await collectionContractWithERC2981.connect(seller).createNFT();
+            await collectionContractWithERC2981
+                .connect(seller)
+                .approve(msgValueMarketplace.address, nftId);
 
-        expect(balanceAfter.sub(balanceBefore)).to.be.closeTo(
-            balanceChange,
-            errorMargin
-        );
-    }
+            await msgValueMarketplace
+                .connect(seller)
+                .list(collectionContractWithERC2981.address, nftId, nftPrice);
+        });
 
-    async function createAndApproveNFT(
-        collection: IERC721 | FusyERC721CollectionWithRoyaltySupport
-    ) {
-        if (isFusyERC721CollectionWithRoyaltySupport(collection)) {
-            await collection.connect(seller).createNFT();
+        async function expectToChangeEtherBalance(
+            trxPromise: (
+                marketplace: Marketplace
+            ) => Promise<ContractTransaction>,
+            target: Address,
+            balanceChange: number | BigNumber,
+            errorMargin: number | BigNumber
+        ) {
+            const balanceBefore = await ethers.provider.getBalance(target);
+            await trxPromise(msgValueMarketplace);
+            const balanceAfter = await ethers.provider.getBalance(target);
+
+            expect(balanceAfter.sub(balanceBefore)).to.be.closeTo(
+                balanceChange,
+                errorMargin
+            );
         }
 
-        await collection.connect(seller).approve(marketplace.address, nftId);
-    }
+        describe("buy()", () => {
+            const royalty = nftPrice.div(10);
 
-    function isFusyERC721CollectionWithRoyaltySupport(
-        collection: IERC721 | FusyERC721CollectionWithRoyaltySupport
-    ): collection is FusyERC721CollectionWithRoyaltySupport {
-        return collection.address === collectionContractWithERC2981.address;
-    }
+            it("should discount royalty from the seller", async () => {
+                const fee = nftPrice.div(50);
 
-    async function listNft(
-        collection: IERC721 | FusyERC721CollectionWithRoyaltySupport
-    ) {
-        return await marketplace
-            .connect(seller)
-            .list(collection.address, nftId, nftPrice);
-    }
-
-    describe("buy()", () => {
-        const royalty = nftPrice.div(10);
-
-        beforeEach(async () => {
-            await createAndApproveNFT(collectionContractWithERC2981);
-            await listNft(collectionContractWithERC2981);
-        });
-
-        it("should discount royalty from the seller", async () => {
-            const fee = nftPrice.div(50);
-
-            await expectToChangeEtherBalance(
-                (m) =>
-                    m
-                        .connect(buyer)
-                        .buy(collectionContractWithERC2981.address, nftId, {
-                            value: nftPrice,
-                        }),
-                seller.address,
-                nftPrice.sub(royalty).sub(fee),
-                ERROR_MARGIN
-            );
-        });
-
-        it("should send royalty to the creator", async () => {
-            await expectToChangeEtherBalance(
-                (m) =>
-                    m
-                        .connect(buyer)
-                        .buy(collectionContractWithERC2981.address, nftId, {
-                            value: nftPrice,
-                        }),
-                creator.address,
-                royalty,
-                ERROR_MARGIN
-            );
-        });
-
-        it("should emit RoyaltyPayment event", async () => {
-            await expect(
-                marketplace
-                    .connect(buyer)
-                    .buy(collectionContractWithERC2981.address, nftId, {
-                        value: nftPrice,
-                    })
-            )
-                .to.emit(marketplace, "RoyaltyPayment")
-                .withArgs(
-                    collectionContractWithERC2981.address,
-                    nftId,
-                    creator.address,
-                    royalty
+                await expectToChangeEtherBalance(
+                    (m) =>
+                        m
+                            .connect(buyer)
+                            .buy(collectionContractWithERC2981.address, nftId, {
+                                value: nftPrice,
+                            }),
+                    seller.address,
+                    nftPrice.sub(royalty).sub(fee),
+                    ERROR_MARGIN
                 );
+            });
+
+            it("should send royalty to the creator", async () => {
+                await expectToChangeEtherBalance(
+                    (m) =>
+                        m
+                            .connect(buyer)
+                            .buy(collectionContractWithERC2981.address, nftId, {
+                                value: nftPrice,
+                            }),
+                    creator.address,
+                    royalty,
+                    ERROR_MARGIN
+                );
+            });
+
+            it("should emit RoyaltyPayment event", async () => {
+                await expect(
+                    msgValueMarketplace
+                        .connect(buyer)
+                        .buy(collectionContractWithERC2981.address, nftId, {
+                            value: nftPrice,
+                        })
+                )
+                    .to.emit(msgValueMarketplace, "RoyaltyPayment")
+                    .withArgs(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        creator.address,
+                        royalty
+                    );
+            });
+        });
+
+        describe("takeCounteroffer()", async () => {
+            const offerPrice = nftPrice.sub(2);
+            const counterofferPrice = offerPrice.add(1);
+            const COUNTER_OFFER_ID = 1;
+            const royalty = counterofferPrice.div(10);
+
+            beforeEach(async () => {
+                await makeOffer();
+                await makeCounteroffer();
+            });
+
+            async function makeOffer() {
+                const DURATION_IN_DAYS = 3;
+                await msgValueMarketplace
+                    .connect(buyer)
+                    .makeOffer(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        DURATION_IN_DAYS,
+                        {
+                            value: offerPrice,
+                        }
+                    );
+            }
+
+            async function makeCounteroffer() {
+                const OFFER_ID = 0;
+                const DURATION_IN_DAYS = 3;
+                await msgValueMarketplace
+                    .connect(seller)
+                    .makeCounteroffer(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        OFFER_ID,
+                        counterofferPrice,
+                        DURATION_IN_DAYS
+                    );
+            }
+
+            it("should discount royalty from the seller", async () => {
+                const fee = counterofferPrice.div(50);
+
+                await expectToChangeEtherBalance(
+                    (m) =>
+                        m.connect(buyer).takeCounteroffer(COUNTER_OFFER_ID, {
+                            value: counterofferPrice,
+                        }),
+                    seller.address,
+                    counterofferPrice.sub(royalty).sub(fee),
+                    ERROR_MARGIN
+                );
+            });
+
+            it("should send royalty to the creator", async () => {
+                await expectToChangeEtherBalance(
+                    (m) =>
+                        m.connect(buyer).takeCounteroffer(COUNTER_OFFER_ID, {
+                            value: counterofferPrice,
+                        }),
+                    creator.address,
+                    royalty,
+                    ERROR_MARGIN
+                );
+            });
+
+            it("should emit RoyaltyPayment event", async () => {
+                await expect(
+                    msgValueMarketplace
+                        .connect(buyer)
+                        .takeCounteroffer(COUNTER_OFFER_ID, {
+                            value: counterofferPrice,
+                        })
+                )
+                    .to.emit(msgValueMarketplace, "RoyaltyPayment")
+                    .withArgs(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        creator.address,
+                        royalty
+                    );
+            });
         });
     });
 
-    describe("takeCounteroffer()", async () => {
-        const offerPrice = nftPrice.sub(2);
-        const counterofferPrice = offerPrice.add(1);
-        const COUNTER_OFFER_ID = 1;
-        const royalty = counterofferPrice.div(10);
-
+    describe("Erc20PaymentMarketplace", () => {
         beforeEach(async () => {
-            await createAndApproveNFT(collectionContractWithERC2981);
-            await listNft(collectionContractWithERC2981);
+            await mintNft();
+            await approveNftSpending();
+            await listNft();
+        });
 
-            await marketplace
-                .connect(buyer)
-                .makeOffer(collectionContractWithERC2981.address, nftId, 3, {
-                    value: offerPrice,
-                });
+        async function mintNft() {
+            await collectionContractWithERC2981.connect(seller).createNFT();
+        }
 
-            await marketplace
+        async function approveNftSpending() {
+            await collectionContractWithERC2981
                 .connect(seller)
-                .makeCounteroffer(
-                    collectionContractWithERC2981.address,
-                    nftId,
-                    0,
-                    counterofferPrice,
-                    3
+                .approve(erc20Marketplace.address, nftId);
+        }
+
+        async function listNft() {
+            await erc20Marketplace
+                .connect(seller)
+                .list(collectionContractWithERC2981.address, nftId, nftPrice);
+        }
+
+        describe("buy()", () => {
+            const royalty = nftPrice.div(10);
+
+            beforeEach(async () => {
+                await payToken
+                    .connect(buyer)
+                    .approve(erc20Marketplace.address, nftPrice);
+            });
+
+            it("should discount royalty from the seller", async () => {
+                const fee = nftPrice.div(50);
+
+                await expect(
+                    erc20Marketplace
+                        .connect(buyer)
+                        .buy(collectionContractWithERC2981.address, nftId)
+                ).to.changeTokenBalance(
+                    payToken,
+                    seller.address,
+                    nftPrice.sub(royalty).sub(fee),
+                    ERROR_MARGIN
                 );
-        });
+            });
 
-        it("should discount royalty from the seller", async () => {
-            const fee = counterofferPrice.div(50);
-
-            await expectToChangeEtherBalance(
-                (m) =>
-                    m.connect(buyer).takeCounteroffer(COUNTER_OFFER_ID, {
-                        value: counterofferPrice,
-                    }),
-                seller.address,
-                counterofferPrice.sub(royalty).sub(fee),
-                ERROR_MARGIN
-            );
-        });
-
-        it("should send royalty to the creator", async () => {
-            await expectToChangeEtherBalance(
-                (m) =>
-                    m.connect(buyer).takeCounteroffer(COUNTER_OFFER_ID, {
-                        value: counterofferPrice,
-                    }),
-                creator.address,
-                royalty,
-                ERROR_MARGIN
-            );
-        });
-
-        it("should emit RoyaltyPayment event", async () => {
-            await expect(
-                marketplace.connect(buyer).takeCounteroffer(COUNTER_OFFER_ID, {
-                    value: counterofferPrice,
-                })
-            )
-                .to.emit(marketplace, "RoyaltyPayment")
-                .withArgs(
-                    collectionContractWithERC2981.address,
-                    nftId,
+            it("should send royalty to the creator", async () => {
+                await expect(
+                    erc20Marketplace
+                        .connect(buyer)
+                        .buy(collectionContractWithERC2981.address, nftId)
+                ).to.changeTokenBalance(
+                    payToken,
                     creator.address,
-                    royalty
+                    royalty,
+                    ERROR_MARGIN
                 );
+            });
+
+            it("should emit RoyaltyPayment event", async () => {
+                await expect(
+                    erc20Marketplace
+                        .connect(buyer)
+                        .buy(collectionContractWithERC2981.address, nftId)
+                )
+                    .to.emit(erc20Marketplace, "RoyaltyPayment")
+                    .withArgs(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        creator.address,
+                        royalty
+                    );
+            });
+        });
+
+        describe("takeCounteroffer()", async () => {
+            const offerPrice = nftPrice.sub(2);
+            const counterofferPrice = offerPrice.add(1);
+            const COUNTER_OFFER_ID = 1;
+            const royalty = counterofferPrice.div(10);
+
+            beforeEach(async () => {
+                await payToken
+                    .connect(buyer)
+                    .approve(erc20Marketplace.address, counterofferPrice);
+
+                await makeOffer();
+                await makeCounteroffer();
+            });
+
+            async function makeOffer() {
+                const DURATION_IN_DAYS = 3;
+                await erc20Marketplace
+                    .connect(buyer)
+                    .makeOffer(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        offerPrice,
+                        DURATION_IN_DAYS
+                    );
+            }
+
+            async function makeCounteroffer() {
+                const OFFER_ID = 0;
+                const DURATION_IN_DAYS = 3;
+                await erc20Marketplace
+                    .connect(seller)
+                    .makeCounteroffer(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        OFFER_ID,
+                        counterofferPrice,
+                        DURATION_IN_DAYS
+                    );
+            }
+
+            it("should discount royalty from the seller", async () => {
+                const fee = counterofferPrice.div(50);
+
+                await expect(
+                    erc20Marketplace
+                        .connect(buyer)
+                        .takeCounteroffer(COUNTER_OFFER_ID)
+                ).to.changeTokenBalance(
+                    payToken,
+                    seller.address,
+                    counterofferPrice.sub(royalty).sub(fee),
+                    ERROR_MARGIN
+                );
+            });
+
+            it("should send royalty to the creator", async () => {
+                await expect(
+                    erc20Marketplace
+                        .connect(buyer)
+                        .takeCounteroffer(COUNTER_OFFER_ID)
+                ).to.changeTokenBalance(
+                    payToken,
+                    creator.address,
+                    royalty,
+                    ERROR_MARGIN
+                );
+            });
+
+            it("should emit RoyaltyPayment event", async () => {
+                await expect(
+                    erc20Marketplace
+                        .connect(buyer)
+                        .takeCounteroffer(COUNTER_OFFER_ID)
+                )
+                    .to.emit(erc20Marketplace, "RoyaltyPayment")
+                    .withArgs(
+                        collectionContractWithERC2981.address,
+                        nftId,
+                        creator.address,
+                        royalty
+                    );
+            });
         });
     });
 });
