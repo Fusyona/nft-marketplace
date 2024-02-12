253790d05570ad1af2e18309f777bcfb0d0a13ec
MakeMake23 <andy.ledesma.garcia@gmail.com>
2024-02-12 18:43:36 -0500

    fix: transfer sFUEL directly; no refund
    Issues: #69

--- scripts/skale-marketplace-wrapper.ts
+++ scripts/skale-marketplace-wrapper.ts
@@ -5,30 +5,23 @@ import {
 } from "@ethersproject/providers";
 import { Address, NotUndefined } from "./marketplace-wrapper";
 import Erc20PaymentMarketplaceWrapper from "./erc20-payment-marketplace-wrapper";
-import {
-    Contract,
-    ContractTransaction,
-    Signer,
-    Wallet,
-    providers,
-} from "ethers";
-import { NebulaFaucet } from "../typechain-types";
+import { BigNumber, Signer, Wallet, providers } from "ethers";
 import { waitAndReturn } from "../utils/transactions";
 
 export default class SkaleMarketplaceWrapper {
     private erc20MarketplaceWrapper: Erc20PaymentMarketplaceWrapper;
     private provider: providers.Web3Provider;
-    private faucet: NebulaFaucet;
     private signer!: JsonRpcSigner;
-    private faucetCaller: Wallet;
+    private funder: Wallet;
+
+    fundingAmount = BigNumber.from("100000000000000");
 
     constructor(
         marketplaceAddress: Address,
         marketplaceAbi: NotUndefined,
         erc20Address: Address,
         erc20Abi: NotUndefined,
-        faucetAddress: Address,
-        faucetCallerPrivateKey: string,
+        funderPrivateKey: string,
         provider: ExternalProvider | JsonRpcFetchFunc,
         private confirmations: number | undefined = undefined
     ) {
@@ -41,22 +34,9 @@ export default class SkaleMarketplaceWrapper {
             provider,
             confirmations
         );
-        this.faucet = this.getFaucetContract(faucetAddress);
-
-        this.faucetCaller = new Wallet(faucetCallerPrivateKey, this.provider);
+        this.funder = new Wallet(funderPrivateKey, this.provider);
 
         this.withSignerIndex(0);
-    }
-
-    private getFaucetContract(faucetAddress: Address): NebulaFaucet {
-        return new Contract(
-            faucetAddress,
-            [
-                "function pay(address payable receiver) external",
-                "function getAmount() external view returns (uint256)",
-            ],
-            this.provider
-        ) as NebulaFaucet;
     }
 
     withSignerIndex(index: number) {
@@ -76,44 +56,26 @@ export default class SkaleMarketplaceWrapper {
     async ensureSFuelAndDo<T>(
         onMarketplace: (_: Erc20PaymentMarketplaceWrapper) => Promise<T>
     ) {
-        await this.ensureSFuel();
-        return onMarketplace(this.erc20MarketplaceWrapper);
-    }
-
-    private async ensureSFuel() {
-        await this.fundFaucetCallerIfNecessary();
         await this.fundUserIfNecessary();
-    }
-
-    private async fundFaucetCallerIfNecessary() {
-        const faucetCallerBalance = await this.faucetCaller.getBalance();
-
-        const amountToClaim = await this.faucet.getAmount();
-        const minFaucetCallerBalance = amountToClaim.div(100);
-
-        if (faucetCallerBalance.lt(minFaucetCallerBalance))
-            await this.getSFuelFor(this.faucetCaller);
-    }
-
-    private async getSFuelFor(beneficiary: Wallet | Signer) {
-        await this.waitAndReturn(
-            this.faucet
-                .connect(this.faucetCaller)
-                .pay(await beneficiary.getAddress())
-        );
-    }
-
-    protected async waitAndReturn(
-        transactionPromise: Promise<ContractTransaction>
-    ) {
-        await waitAndReturn(transactionPromise, this.confirmations);
+        return onMarketplace(this.erc20MarketplaceWrapper);
     }
 
     private async fundUserIfNecessary() {
         const userBalance = await this.signer.getBalance();
-        const amountToClaim = await this.faucet.getAmount();
-        if (userBalance.lt(amountToClaim)) {
+        if (userBalance.lt(this.fundingAmount)) {
             await this.getSFuelFor(this.signer);
         }
+    }
+
+    private async getSFuelFor(beneficiary: Wallet | Signer) {
+        const currentBalance = await beneficiary.getBalance();
+        const sFuelToSend = this.fundingAmount.sub(currentBalance);
+        await waitAndReturn(
+            this.funder.sendTransaction({
+                to: await beneficiary.getAddress(),
+                value: sFuelToSend,
+            }),
+            this.confirmations
+        );
     }
 }
