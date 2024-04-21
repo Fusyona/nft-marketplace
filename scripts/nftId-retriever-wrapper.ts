7e083e922c479751a3684e330cc0bbf13d067c17
daryckont <115124234+daryckont@users.noreply.github.com>
2024-04-21 13:45:37 -0400

    chore: modificate nftId-retriever-wrapper.ts

--- scripts/nftId-retriever-wrapper.ts
+++ scripts/nftId-retriever-wrapper.ts
@@ -1,33 +1,44 @@
 import { ExternalProvider, JsonRpcFetchFunc } from "@ethersproject/providers";
 import { BigNumber, Contract, providers, ethers } from "ethers";
-import { Address, NotUndefined } from "./types";
-import { NftIdRetriever } from "../typechain-types";
+import { Address } from "./types";
+import NftIdRetrieverArtifact from "../artifacts/contracts/NftIdRetriever.sol/NftIdRetriever.json";
+import ERC721Artifact from "../artifacts/@openzeppelin/contracts/token/ERC721/ERC721.sol/ERC721.json";
+import { NftIdRetriever, ERC721 } from "../typechain-types";
 
 export default class NftIdRetrieverWrapper {
     private nftIdRetriever: NftIdRetriever;
     private provider: ethers.providers.Web3Provider;
+    private erc721Contract: ERC721;
+    private collectionAddress: Address;
 
     constructor(
         tokenIdRetrieverAddress: Address,
-        tokenIdRetrieverAbi: NotUndefined,
+        collectionAddress: Address,
         provider: ExternalProvider | JsonRpcFetchFunc
     ) {
         this.provider = this.provider = new providers.Web3Provider(provider);
+        this.collectionAddress = collectionAddress;
+
         this.nftIdRetriever = new Contract(
             tokenIdRetrieverAddress,
-            tokenIdRetrieverAbi,
+            NftIdRetrieverArtifact.abi,
             this.provider
         ) as NftIdRetriever;
+
+        this.erc721Contract = new Contract(
+            collectionAddress,
+            ERC721Artifact.abi,
+            this.provider
+        ) as ERC721;
     }
 
     async tokensOfOwner(
-        collection: Address,
         owner: Address,
         startId: BigNumber | number,
         endId: BigNumber | number
     ) {
         const ownedIds = await this.nftIdRetriever.tokensOfOwner(
-            collection,
+            this.collectionAddress,
             owner,
             startId,
             endId
@@ -35,4 +46,16 @@ export default class NftIdRetrieverWrapper {
         const idsToNumber = ownedIds.map((id) => id.toNumber());
         return idsToNumber;
     }
+
+    async getTokenNameAndSymbol() {
+        let name = await this.erc721Contract.name();
+        let symbol = await this.erc721Contract.symbol();
+
+        return { name, symbol };
+    }
+
+    async getTokenURI(tokenId: BigNumber | number) {
+        return await this.erc721Contract.tokenURI(tokenId);
+    }
+
 }
